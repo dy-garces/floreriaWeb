703900eadb8347c22fa03cb42a1b1e558e054609
Dyllan Garces <dy.garces@duocuc.cl>
2022-05-29 14:05:52 -0400

    traspaso de paginas a django

--- floreria/floreria/aplicacionfloreria/views.py
+++ floreria/floreria/aplicacionfloreria/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'aplicacionfloreria/home.html')
+
+def flores(request):
+    return render(request,'aplicacionfloreria/flores.html')
+
+def plantas(request):
+    return render(request,'aplicacionfloreria/plantas.html')
+
+def arboles(request):
+    return render(request,'aplicacionfloreria/arboles.html')
+
+def maceteros(request):
+    return render(request,"aplicacionfloreria/maceteros.html")
+
+def jardineria(request):
+    return render(request,"aplicacionfloreria/jardineria.html")
+
+def contacto(request):
+    return render(request,"aplicacionfloreria/contacto.html")
+
+def quienesSomos(request):
+    return render(request,"aplicacionfloreria/quienesSomos.html")

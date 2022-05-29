6d469b008e5396cae2abe5674806f77a77248093
Dyllan Garces <dy.garces@duocuc.cl>
2022-05-29 19:59:05 -0400

    agregada tablas y poblado de tablas producto y categoria

--- floreria/floreria/aplicacionfloreria/models.py
+++ floreria/floreria/aplicacionfloreria/models.py
@@ -1,3 +1,30 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.CharField(max_length=200)
+    imgen = models.ImageField(upload_to="productos",null=True)
+    categoria =  models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Region(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=50)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
--- floreria/floreria/aplicacionfloreria/views.py
+++ floreria/floreria/aplicacionfloreria/views.py
@@ -1,26 +1,60 @@
+from itertools import product
 from django.shortcuts import render
+from .models import Categoria, Producto
 
 # Create your views here.
-def home(request):
+def home(request):   
     return render(request,'aplicacionfloreria/home.html')
 
 def flores(request):
-    return render(request,'aplicacionfloreria/flores.html')
+    productos = Producto.objects.all()
+    categoria = Categoria.objects.all()
+    contexto = {
+        'productos': productos,
+        'categoria': categoria
+    }
+    return render(request,'aplicacionfloreria/flores.html',contexto)
 
 def plantas(request):
-    return render(request,'aplicacionfloreria/plantas.html')
+    productos = Producto.objects.all()
+    categoria = Categoria.objects.all()
+    contexto = {
+        'productos': productos,
+        'categoria': categoria
+    }
+    return render(request,'aplicacionfloreria/plantas.html',contexto)
 
 def arboles(request):
-    return render(request,'aplicacionfloreria/arboles.html')
+    productos = Producto.objects.all()
+    categoria = Categoria.objects.all()
+    contexto = {
+        'productos': productos,
+        'categoria': categoria
+    }
+    return render(request,'aplicacionfloreria/arboles.html',contexto)
 
 def maceteros(request):
-    return render(request,"aplicacionfloreria/maceteros.html")
+    productos = Producto.objects.all()
+    categoria = Categoria.objects.all()
+    contexto = {
+        'productos': productos,
+        'categoria': categoria
+    }
+    return render(request,"aplicacionfloreria/maceteros.html",contexto)
 
 def jardineria(request):
-    return render(request,"aplicacionfloreria/jardineria.html")
+    productos = Producto.objects.all()
+    categoria = Categoria.objects.all()
+    contexto = {
+        'productos': productos,
+        'categoria': categoria
+    }
+    return render(request,"aplicacionfloreria/jardineria.html",contexto)
 
 def contacto(request):
+
     return render(request,"aplicacionfloreria/contacto.html")
 
 def quienesSomos(request):
+
     return render(request,"aplicacionfloreria/quienesSomos.html")
